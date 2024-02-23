2d40b4a03424368cc70387aa5ca02eb4c961b712
Shilpa Monde <shilpas1003@gmail.com>
2024-02-24 02:49:07 +0530

    added gitigonore

--- sum.py
+++ sum.py
@@ -1,18 +1,19 @@
 # Function to perform sum of two variables
-def sum_of_variables(a, b):
-    return a + b
+def sum_of_variables(a, b,c):
+    return a + b + c
 
 # Main function
 def main():
     # Taking input from user
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = float(input("Enter the first num: "))
+    num2 = float(input("Enter the second num: "))
+    num3 = float(input("Enter the Third num: "))
 
-    # Calculating sum
-    result = sum_of_variables(num1, num2)
+    # Calculating sum of 3 var
+    result = sum_of_variables(num1, num2, num3)
 
     # Displaying the result
-    print("The sum of", num1, "and", num2, "is:", result)
+    print("The sum of", num1, "and", num2, "and" ,num3, "is:", result)
 
 if __name__ == "__main__":
     main()
