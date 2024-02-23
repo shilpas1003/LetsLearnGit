6487f5199116f56d8c344cc02354490ff8dc4e9c
Shilpa Monde <shilpas1003@gmail.com>
2024-02-24 01:08:25 +0530

    diff Initial Commit

---
+++ diff.py
@@ -0,0 +1,18 @@
+# Function to perform sum of two variables
+def diff_of_variables(a, b):
+    return a - b
+
+# Main function
+def main():
+    # Taking input from user
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    # Calculating sum
+    result = diff_of_variables(num1, num2)
+
+    # Displaying the result
+    print("The diff of", num1, "and", num2, "is:", result)
+
+if __name__ == "__main__":
+    main()
